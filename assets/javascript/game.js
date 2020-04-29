c35ba8edad8170c15dde23dddb9ee294c852b5fb
Sandra Smith <toosandra@gmail.com>
2020-04-28 23:04:44 -0400

    Created pseudo code in js file.

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+//Much like Word Guess Game, instead player will guess numbers.
+
+//Display 4 images of crystals(red, purple, orange, yellow (image file).
+//Each crystal to have random # "hidden" values. ARRAY: 1-12.
+//Player shown RANDOM # at start of game. ARRAY: 19-120. FO LOOP?
+//Use ON(CLICK) for each Crystal, # RANDOMLY chosen (ARRAY). FOR LOOP?
+//Create TOTAL SCORE. Points to INCREMENTAL add.
+    //Player clicks on crystal, VALUE is hidden, but added to the TOTAL SCORE. FOR Loop?
+//Wins IF total score === (matches) the random number from the start of game.
+//Loses IF total score > the random number.
+//Game RESTARTS if win or loss, and new random number appears. Crystals to have new hidden values. FOR LOOP?
+//User's score will RESET to 0.
+//Overall score...Display WINS and LOSSES, FOR LOOP. 
+//Game automatically goes into another GAME -- Overal score is kept and tracked. But values are refreshed.
+
+
+
