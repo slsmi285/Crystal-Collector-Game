7658cedeb190f6202ea6c6f3b62baf5d7b8de3a0
Sandra Smith <toosandra@gmail.com>
2020-04-28 23:37:18 -0400

    Creating buttons in html. Creating code on js.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,23 @@
 //Much like Word Guess Game, instead player will guess numbers.
 
 //Display 4 images of crystals(red, purple, orange, yellow (image file).
+var yourScore = getElementById(red, purple, yellow, orange.onclick(math.sum));
+console.log("line 4", yourScore);
+yourScore.innerHTML = 0;
+console.log("line 7", yourScore);
+var targetScore = document.getElementById("targetScore");
+console.log("line 9", targetScore);
+targetScore = getRandomValue(1-12);
+console.log("line 11", targetScore);
 //Each crystal to have random # "hidden" values. ARRAY: 1-12.
 //Player shown RANDOM # at start of game. ARRAY: 19-120. FO LOOP?
 //Use ON(CLICK) for each Crystal, # RANDOMLY chosen (ARRAY). FOR LOOP?
 //Create TOTAL SCORE. Points to INCREMENTAL add.
     //Player clicks on crystal, VALUE is hidden, but added to the TOTAL SCORE. FOR Loop?
-//Wins IF total score === (matches) the random number from the start of game.
+var yourScore = document.getElementById("yourScore");{
+    console.log("line 15", yourScore);
+}
+    //Wins IF total score === (matches) the random number from the start of game.
 //Loses IF total score > the random number.
 //Game RESTARTS if win or loss, and new random number appears. Crystals to have new hidden values. FOR LOOP?
 //User's score will RESET to 0.
